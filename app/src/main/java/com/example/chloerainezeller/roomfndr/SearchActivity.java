7915083e741fd105df213282517452b5bb07284a
Unknown <czeller@oxy.edu>
2018-04-10 21:18:24 -0700

    Added more detail to search activity

--- app/src/main/java/com/example/chloerainezeller/roomfndr/SearchActivity.java
+++ app/src/main/java/com/example/chloerainezeller/roomfndr/SearchActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -16,6 +17,7 @@ public class SearchActivity extends AppCompatActivity {
     private Spinner buildingSpinner;
     private Spinner floorSpinner;
     private Button favoritesButton;
+    private Button searchButton;
     private ImageButton backButton;
     private ListView roomListView;
 
@@ -29,10 +31,36 @@ public class SearchActivity extends AppCompatActivity {
         floorSpinner = findViewById(R.id.floorSpinner);
         favoritesButton = findViewById(R.id.favoritesButton);
         backButton = findViewById(R.id.backButtonSearch);
+        searchButton = findViewById(R.id.searchButton);
+        roomListView = findViewById(R.id.searchResultsListView);
 
         // TODO: Configure spinner options
-        // TODO: Configure onItemClick
-        // TODO: Decide if we want a search button, or to populate search based on spinner clicks
+        /*
+        * Seems like we should only feature some of the buildings on campus in our search:
+        *   - Fowler
+        *   - Johnson
+        *   - Berkus/Rangeview (stored under both names of fucking course)
+        *   - Booth
+        *   - Hameetman Science Center
+        *   - Johnson Student Center
+        *   - Library
+        *   - Weingart
+        * Then maybe automatically refine the number of floors or whatever given the entry from
+        * the first dropdown. So like Fowler goes to all three floors, but the JSC gives all its
+        * weird room names. We can build a hashmap from the building to the available floors/rooms
+        * */
+
+
+
+        // TODO: Configure onItemClick on roomListView
+        roomListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // TODO: package an intent with the room information
+
+            }
+        });
+        // TODO: Configure search button method below
 
 
     }
@@ -48,5 +76,10 @@ public class SearchActivity extends AppCompatActivity {
     public void goBack(View view) {
         Intent intent = new Intent(myContext, MainActivity.class);
         startActivity(intent);
+    }
+
+    // search button, searches all available rooms
+    public void search(View view) {
+        // FIXME: populate the list view with the search results, going to need an adapter
     }
 }
---
+++ app/src/main/java/com/example/chloerainezeller/roomfndr/SearchResultActivity.java
@@ -0,0 +1,20 @@
+package com.example.chloerainezeller.roomfndr;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SearchResultActivity extends AppCompatActivity {
+
+    private Context myContext;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        myContext = this;
+
+
+    }
+}
