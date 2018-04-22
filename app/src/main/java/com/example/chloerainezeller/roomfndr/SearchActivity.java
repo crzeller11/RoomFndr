c572f876ff94fb7d33d4892320b2d00449d293f5
Unknown <czeller@oxy.edu>
2018-04-21 23:48:22 -0700

    Beginning search functionality
    Parsed the JSON file as a Reservation object, added a method to SearchActivity that creates a HashMap that maps from room to an arraylist of all reservations in that room, and added a subsidized list of rooms to the search result options

--- app/src/main/java/com/example/chloerainezeller/roomfndr/SearchActivity.java
+++ app/src/main/java/com/example/chloerainezeller/roomfndr/SearchActivity.java
@@ -8,88 +8,67 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Spinner;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class SearchActivity extends AppCompatActivity {
 
     private Context myContext;
-    private Spinner buildingSpinner;
-    private Spinner floorSpinner;
+
     private Button favoritesButton;
-    private Button searchButton;
     private ImageButton backButton;
+
+    private EditText buildingEditText;
+    private EditText floorEditText;
+    private Button searchButton;
     private ListView roomListView;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_activity);
-
+        System.out.println("REACHING THIS ACTIVITY!");
 
         myContext = this;
-        buildingSpinner = findViewById(R.id.buildingSpinner);
-        floorSpinner = findViewById(R.id.floorSpinner);
         favoritesButton = findViewById(R.id.favoritesButton);
         backButton = findViewById(R.id.backButtonSearch);
+        buildingEditText = findViewById(R.id.buildingEditText);
+        floorEditText = findViewById(R.id.floorEditText);
         searchButton = findViewById(R.id.searchButton);
         roomListView = findViewById(R.id.searchResultsListView);
 
-        // TODO: Configure spinner options
-        /*
-        * Seems like we should only feature some of the buildings on campus in our search:
-        *   - Fowler
-        *   - Johnson
-        *   - Berkus/Rangeview (stored under both names of fucking course)
-        *   - Booth
-        *   - Hameetman Science Center
-        *   - Johnson Student Center
-        *   - Library
-        *   - Weingart
-        *   - Hinchcliffe Hall
-        *   #hello this is allie
-        *
-        * Then maybe automatically refine the number of floors or whatever given the entry from
-        * the first dropdown. So like Fowler goes to all three floors, but the JSC gives all its
-        * weird room names. We can build a hashmap from the building to the available floors/rooms
-        * */
-
-        ArrayList<String> buildingOptions = new ArrayList<>();
-
-        buildingOptions.add("");
-        buildingOptions.add("Fowler");
-        buildingOptions.add("Johnson");
-        buildingOptions.add("Berkus/Rangeview");
-        buildingOptions.add("Booth");
-        buildingOptions.add("Hameetman Science Center");
-        buildingOptions.add("Johnson Student Center");
-        buildingOptions.add("Library");
-        buildingOptions.add("Weingart");
-        buildingOptions.add("Swan/Hinchcliffe");
+        // a list of all the reservations for the day
+        ArrayList<Reservation> dailyReservations = Reservation.getReservationsFromFile(
+                "reservations.json", this);
+        System.out.println(dailyReservations.get(0).reservationDescription);
 
 
-        // FIXME: add these options using a HashMap
-        //ArrayList<String> floorOptions = findFloorOptions();
+        //ArrayList<String> searchResults = findSearchResults();
 
-        ArrayAdapter<String> buildingOptionsAdapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_dropdown_item, buildingOptions);
+        // FIXME: Consider an EditText Option to do the search
+        //ReservationAdapter adapter = new ResultAdapter(this, searchResults);
 
-        buildingSpinner.setAdapter(buildingOptionsAdapter);
 
         /*
         // TODO: Configure onItemClick on roomListView
         roomListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // TODO: package an intent with the room information
-                Intent intent = new Intent(myContext, SearchResultActivity.class);
-                // FIXME: add shit to the intent, pass to result
+                Reservation selectedReservation = reservation
+
+                //Intent intent = new Intent(myContext, SearchResultActivity.class);
 
                 // startActivity(intent);
 
@@ -114,29 +93,61 @@ public class SearchActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    /*
 
+    public HashMap<String, ArrayList> roomOccupation(ArrayList<Reservation> dailyReservations) {
+        HashMap<String, ArrayList> roomOccupationMap = new HashMap<>();
+        ArrayList<String> allRooms = new ArrayList<>(Arrays.asList(
+                "BIOS 105", "BIOS 106", "BIOS 111", "BIOS 113", "BIOS 200", "BIOS 210",
+                "BIOS 309", "BIOS 311", "BOOTH 118", "BOOTH 119", "BOOTH 204", "BOOTH 208",
+                "BOOTH 223A", "BOOTH 223B", "BOOTH 224", "BOOTH 226", "FOWLER 108", "FOWLER 110",
+                "FOWLER 111", "FOWLER 112", "FOWLER 113", "FOWLER 201", "FOWLER 202", "FOWLER 207",
+                "FOWLER 208", "FOWLER 209", "FOWLER 210", "FOWLER 301", "FOWLER 302", "FOWLER 307",
+                "FOWLER 309", "FOWLER 310", "FOWLER 311 (MATH REFERENCE ROOM)", "HERRICK LOWER",
+                "HERRICK UPPER", "HSC 004", "HSC 015", "HSC 102", "HSC 104", "HSC 124", "HSC 202",
+                "HSC 203", "HSC 210", "HSC 225", "JOHN 100", "JOHN 103", "JOHN 104", "JOHN 105",
+                "JOHN 106", "JOHN 200", "JOHN 203", "JOHN 301", "JOHN 302", "JOHN 303",
+                "JOHN 313 - SKYBOX", "JOHN 314 - SKYBOX", "JOHN 315", "JSC BENGAL ROOM",
+                "JSC COMMONS", "JSC GRESHAM", "JSC MORRISON", "JSC SALSBURY", "JSC SALSBURY-YOUNG",
+                "JSC STUDENMUND CONFERENCE ROOM", "KECK 208", "KECK 216", "LIB - AHMANSON READING ROOM",
+                "LIB 116", "LIB 163", "LIB 220", "LIB 251", "LIB 258 - JEFFERS", "LIB 260", "LIB 3",
+                "LIB 355", "LIB BRAUN", "LIB BROWN 219", "LIB BROWN 219 OUTSIDE", "LIB CAE LAB",
+                "LIB CUMBERLAND ROOM", "LIB OMAC LAB", "LIB PC1", "MOSHER 1", "MOSHER 2", "MOSHER 3",
+                "NORRIS 101", "NORRIS 105", "NORRIS 106", "NORRIS 110", "NORRIS 200", "NORRIS 202",
+                "NORRIS 203", "NORRIS 204","PAULEY HALL - MLK LOUNGE", "PAULEY PATIO",
+                "PRESIDENT'S MITCHELL GARDEN", "PRESIDENT'S PATIO", "PRESIDENT'S ROSE GDN",
+                "R.V. 201", "R.V. 206", "R.V. 217", "RANGEV 232", "RANGEV 242", "RANGEV 243",
+                "RANGEV 243B", "RANGEV 244", "SWAN 227", "SWAN N 200", "SWAN SOUTH 119", "SYCAMORE GLEN",
+                "THE CHOI AUDITORIUM", "VARELAS INNOVATION LAB", "WEIN 10", "WEIN 110", "WEIN 116",
+                "WEIN 117", "WEIN 209", "WEIN 210", "WEIN LAWN", "WEINGART - N. GALLERY RM. 103",
+                "WEINGART E. GALLERY RM. 100"));
+
+        for (int i = 0; i < allRooms.size(); i++) {
+            String currentRoom = allRooms.get(i);
+            ArrayList<Reservation> roomsReservations = new ArrayList<>();
+            for (int j = 0; j < dailyReservations.size(); j++) {
+                Reservation currentReservation = dailyReservations.get(j);
+                if (currentReservation.roomAssignment.equals(currentRoom)) {
+                    roomsReservations.add(currentReservation);
+                }
+            }
+            roomOccupationMap.put(currentRoom, roomsReservations);
+        }
+
+        return roomOccupationMap;
+    }
+
+    /*
     // search button, searches all available rooms
     public void searchReservations(View view) {
-        // FIXME: populate the list view with the search results, going to need an adapter
-    }
+        String roomSelection = buildingEditText.getText().toString();
+        String floorSelection = floorEditText.getText().toString();
 
-    public ArrayList<String> findFloorOptions() {
-        String buildingSelection = buildingSpinner.getSelectedItem().toString();
-        HashMap<String, ArrayList<String>> buildingHashmap = createBuildingHashmap();
-
-        ArrayList<String> floorOptions = buildingHashmap.get(buildingSelection);
-
-        return floorOptions;
+        HashMap<String, String> buildingHashMap = generateBuildingMap();
+        // Probably need a hashmap from edit text entries to the building code it has
+    
 
     }
+*/
 
-    public HashMap<String, ArrayList<String>> createBuildingHashmap() {
-        // TODO: Create a method that maps from a building to it's subsections, whether that be floors or rooms
-        HashMap<String, ArrayList<String>> buildingHashmap = new HashMap<>();
-        // FIXME
-        return buildingHashmap;
-    }
-    */
 
 }
