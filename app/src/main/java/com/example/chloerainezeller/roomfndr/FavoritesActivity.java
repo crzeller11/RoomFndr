530062a7809ae54b84eee4e1596fbf869ce11990
Allie Brenner <abrenner@oxy.edu>
2018-05-05 21:19:14 -0700

    favorites activity complete

--- app/src/main/java/com/example/chloerainezeller/roomfndr/FavoritesActivity.java
+++ app/src/main/java/com/example/chloerainezeller/roomfndr/FavoritesActivity.java
@@ -1,12 +1,17 @@
 package com.example.chloerainezeller.roomfndr;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -15,24 +20,77 @@ import java.util.ArrayList;
 public class FavoritesActivity extends AppCompatActivity {
     private Context myContext;
     private ListView favoriteRoomsListView;
-    private DatabaseHelper myDb;
-    private TextView roomName;
-    private ArrayList<String>myList;
+    private TextView availability;
+    private MyAdapter adapter;
+    private ImageButton addNewFav;
+
+    dbHelper myDb;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.favorites_activity);
+        myDb = new dbHelper(this);
         myContext = this;
 
-/*
-        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.favorites_activity,myList);
+        Intent intent=getIntent();
+        ArrayList rooms = intent.getStringArrayListExtra("room_list");
+        System.out.println(rooms);
+
+        Cursor res = myDb.getAllData();
+        if (res.getCount() == 0){
+            System.out.println("No data found");
+            return;
+        }
+
+        ArrayList<String>myList= new ArrayList<>();
+        while (res.moveToNext()){
+            myList.add(res.getString(1 )+"\n");
+        }
+
+        final ArrayList<String>idList= new ArrayList<>();
+        while(res.moveToNext()){
+            idList.add(res.getString(0)+"\n");
+        }
+
+
         favoriteRoomsListView = findViewById(R.id.myFavoritesListView);
+        availability=findViewById(R.id.availabilityListItem);
+
+        adapter = new FavoritesActivity.MyAdapter(this,myList);
         favoriteRoomsListView.setAdapter(adapter);
-*/
+
+        favoriteRoomsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Object itemName = favoriteRoomsListView.getItemAtPosition(position);
+                String selectedRoom = itemName.toString();
+
+                Intent detailIntent = new Intent(myContext, SearchResultActivity.class);
+
+                detailIntent.putExtra("roomName", selectedRoom);
+                /*
+                //if (openRoom.contains(selectedRoom)) {
+               // detailIntent.putExtra("availability","Available");}
+                //else{
+                //detailIntent.putExtra("availability","Not Available");}
+                startActivity(detailIntent);*/
+            }
+        });
+
+
+        addNewFav=findViewById(R.id.addNewFavoriteRoom);
+        addNewFav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(myContext,FavoriteAddActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-    //favoriteRoomsListView = findViewById(R.id.myFavoritesListView);
+
+
     public void goBack(View view) {
         Intent intent = new Intent(myContext, SearchActivity.class);
         startActivity(intent);
@@ -43,14 +101,58 @@ public class FavoritesActivity extends AppCompatActivity {
         Intent intent = new Intent(myContext, FavoritesActivity.class);
         startActivity(intent);
 
-        Cursor todoCursor = myDb.getAllData();
-        if (todoCursor.getCount() == 0){
-            roomName.setText("No data found.");
-            return;
-        }
-        //StringBuffer buffer = new StringBuffer();
-        while (todoCursor.moveToNext()){
-            myList.add(todoCursor.getString(1 )+"\n");}
-        roomName.setText(myList.toString());
     }
+
+
+    public class MyAdapter extends BaseAdapter {
+
+        ArrayList<String> arrayList;
+        LayoutInflater inflater;
+
+        public MyAdapter(Context context, ArrayList<String> arrayList) {
+            this.arrayList = arrayList;
+            inflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public int getCount() {
+            return arrayList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return arrayList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        private class ViewHolder {
+            TextView textView;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            FavoritesActivity.MyAdapter.ViewHolder holder = null;
+
+            if (convertView == null) {
+
+                holder = new FavoritesActivity.MyAdapter.ViewHolder();
+                convertView = inflater.inflate(R.layout.favorites_list_item, null);
+                holder.textView = convertView.findViewById(R.id.roomNumberListItem);
+                convertView.setTag(holder);
+            } else {
+                holder = (FavoritesActivity.MyAdapter.ViewHolder) convertView.getTag();
+            }
+            holder.textView.setText(arrayList.get(position));
+            return convertView;
+        }
+
+
+
+    }
+
 }
